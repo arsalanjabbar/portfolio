99a7934ac08cfd5447a0f25b6b50dcd44572b23b
arsalanjabbar <arsalan.j.memon@gmail.com>
2026-08-11 00:53:45 +0500

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const topbar=document.querySelector('.topbar'),menu=document.querySelector('.menu');
-menu?.addEventListener('click',()=>{const open=topbar.classList.toggle('nav-open');menu.setAttribute('aria-expanded',open)});
-document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>topbar.classList.remove('nav-open')));
